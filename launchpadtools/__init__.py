8b444df1aa593b8131ff6bf6da189fd0fb026ce0
Nico Schlömer <nico.schloemer@gmail.com>
2018-08-27 22:28:37 +0200

    use black formatting

--- launchpadtools/__init__.py
+++ launchpadtools/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 
-from launchpadtools.__about__ import (
-    __version__,
-    __author__,
-    __author_email__,
-    )
+from launchpadtools.__about__ import __version__, __author__, __author_email__
 
 from . import submit
--- setup.py
+++ setup.py
@@ -7,49 +7,43 @@ import codecs
 # https://packaging.python.org/single_source_version/
 base_dir = os.path.abspath(os.path.dirname(__file__))
 about = {}
-with open(os.path.join(base_dir, 'launchpadtools', '__about__.py'), 'rb') as f:
+with open(os.path.join(base_dir, "launchpadtools", "__about__.py"), "rb") as f:
     exec(f.read(), about)
 
 
 def read(fname):
     try:
         content = codecs.open(
-            os.path.join(os.path.dirname(__file__), fname),
-            encoding='utf-8'
-            ).read()
+            os.path.join(os.path.dirname(__file__), fname), encoding="utf-8"
+        ).read()
     except Exception:
-        content = ''
+        content = ""
     return content
 
 
 setup(
-    name='launchpadtools',
-    version=about['__version__'],
-    author=about['__author__'],
-    author_email=about['__author_email__'],
+    name="launchpadtools",
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
     packages=find_packages(),
-    description='Tools for Debian/Ubuntu Launchpad',
-    long_description=read('README.rst'),
-    url='https://github.com/nschloe/launchpadtools',
-    download_url='https://pypi.python.org/pypi/launchpadtools',
-    license=about['__license__'],
-    platforms='any',
-    install_requires=[
-        'GitPython',
-        'launchpadlib',
-        ],
+    description="Tools for Debian/Ubuntu Launchpad",
+    long_description=read("README.rst"),
+    url="https://github.com/nschloe/launchpadtools",
+    download_url="https://pypi.python.org/pypi/launchpadtools",
+    license=about["__license__"],
+    platforms="any",
+    install_requires=["GitPython", "launchpadlib"],
     classifiers=[
-        about['__status__'],
-        about['__license__'],
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: System :: Operating System'
-        ],
-    scripts=[
-        'tools/launchpad-submit',
-        ]
-    )
+        about["__status__"],
+        about["__license__"],
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: System :: Operating System",
+    ],
+    scripts=["tools/launchpad-submit"],
+)
