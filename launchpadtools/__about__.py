b954e901ee4df18a480e5a38ac16507e240321ba
Nico Schlömer <nico.schloemer@gmail.com>
2018-08-27 19:54:59 +0200

    version bump

--- launchpadtools/__about__.py
+++ launchpadtools/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = '0.6.5'
+__version__ = '0.6.6'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __status__ = 'Development Status :: 4 - Beta'
