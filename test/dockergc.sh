3334c4a468b1b008547c7c2f576c177f2f1a3a24
Nico Schlömer <nico.schloemer@gmail.com>
2019-05-29 12:40:13 +0200

    fix test

--- test/dockergc.sh
+++ test/dockergc.sh
@@ -4,7 +4,6 @@ TMP_DIR="/tmp/dockergc-test-launchpadtools/"
 rm -rf "$TMP_DIR"
 mkdir "$TMP_DIR"
 
-ORIG_DIR="$TMP_DIR/orig"
 CACHE="$HOME/.cache/repo/docker-gc"
 git -C "$CACHE" pull || git clone "https://github.com/spotify/docker-gc.git" "$CACHE"
-git clone --shared "$CACHE" "$ORIG_DIR"
+git clone --shared "$CACHE" "$TMP_DIR"
